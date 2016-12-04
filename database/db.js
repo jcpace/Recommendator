7b3a53b11167fa3c08b0db82a057dd170661d137
Joshua Pace <joshpace@gmail.com>
2016-12-03 18:04:35 -0800

    [add]

--- database/db.js
+++ database/db.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema
 
 // Schema
 const userSchema = new Schema({
-<<<<<<< HEAD
   username: String,
   email: String,
   password: String,
@@ -27,33 +26,6 @@ const favoriteSchema = new Schema({
   businessUrl: String,
   lat: Number,
   long: Number
-=======
-  auth: {
-    username: String,
-    email: String,
-    password: String,
-    fbAuth: Boolean
-  },
-  profile: [{
-    name: String,
-    bio: String,
-    photo: String,
-    hometown: String,
-    interests: String
-  }],
-  favorites: [{
-    yelpId: String,
-    name: String,
-    categories: [String],
-    address: String,
-    phone: String,
-    rating: Number,
-    image_url: String,
-    businessUrl: String,
-    lat: Number,
-    long: Number
-  }]
->>>>>>> [merge] updated schema
 })
 const commentSchema = new Schema({
   userId: String,
